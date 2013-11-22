c28458fe35091339eaef3fe0bc6c0382c58ef117
Ingo Oeser <ingo@jimdo.com>
2013-11-22 23:05:10 +0100

    better docs

--- changelog/changelog.go
+++ changelog/changelog.go
@@ -1,3 +1,5 @@
+// Package changelog provides helpers to parse a typical
+// Debian changelog file found in packages generated from source packages.
 package changelog
 
 import (
@@ -8,6 +10,7 @@ import (
 	"unicode"
 )
 
+// Change describes a single changeset between 2 versions
 type Change struct {
 	Source  string
 	Version string
@@ -19,9 +22,11 @@ type Change struct {
 	Changes []byte
 }
 
+// Change describes a full or partial changelog
 type Changelog []Change
 
-func Parse(r io.Reader, beforeVersion string) (Changelog, error) {
+// Parse a debian changelog from r for any changes happening later than afterVersion
+func Parse(r io.Reader, afterVersion string) (Changelog, error) {
 	scanner := bufio.NewScanner(r)
 	changelog := make(Changelog, 0, 5)
 	change := Change{}
@@ -46,7 +51,7 @@ func Parse(r io.Reader, beforeVersion string) (Changelog, error) {
 			s, v, d, u := f[0], f[1], f[2], f[3]
 			change.Version = string(bytes.Trim(v, "()"))
 
-			if len(beforeVersion) > 0 && change.Version == beforeVersion {
+			if len(afterVersion) > 0 && change.Version == afterVersion {
 				break
 			}
 
