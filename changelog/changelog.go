79ac1ce691ab3ad15230359a075ec9e7e874f2cc
Ingo Oeser <ingo@jimdo.com>
2013-11-29 16:21:36 +0100

    split out parsing line and author, document bug

--- changelog/changelog.go
+++ changelog/changelog.go
@@ -26,6 +26,44 @@ type Change struct {
 // Changelog describes a full or partial changelog
 type Changelog []Change
 
+func (change *Change) parseVersionLine(b []byte) (ok bool) {
+
+	// FIXME: This is wrong, because Dist is actually a space seperated list
+	f := bytes.Fields(b)
+	if len(f) < 4 {
+		return false
+	}
+	s, v, d, u := f[0], f[1], f[2], f[3]
+	change.Version = string(bytes.Trim(v, "()"))
+
+	change.Source = string(s)
+	change.Dist = string(bytes.TrimRight(d, ";"))
+	change.Urgency = string(bytes.TrimPrefix(u, []byte("urgency=")))
+
+	return change.Version != "" && change.Source != "" && change.Dist != ""
+}
+
+func (change *Change) parseChangedByLine(b []byte) (ok bool) {
+	if !bytes.HasPrefix(b, []byte(" -- ")) {
+		return false
+	}
+	line := bytes.TrimPrefix(b, []byte(" -- "))
+	start := bytes.IndexByte(line, '<')
+	end := bytes.IndexByte(line, '>')
+	if start < 0 || start >= end || end >= len(line) {
+		return false
+	}
+	update, err := time.Parse(time.RFC1123Z, string(bytes.TrimSpace(line[end+1:])))
+	if err != nil {
+		return false
+	}
+	change.Author = string(bytes.TrimSpace(line[:start]))
+	change.Email = string(line[start+1 : end-2])
+	change.Changed = update
+
+	return true
+}
+
 // Parse a debian changelog from r for any changes happening later than afterVersion
 func Parse(r io.Reader, afterVersion string) (Changelog, error) {
 	scanner := bufio.NewScanner(r)
@@ -44,44 +82,18 @@ func Parse(r io.Reader, afterVersion string) (Changelog, error) {
 			}
 			continue
 		}
-		if !inside {
-			f := bytes.Fields(b)
-			if len(f) < 4 {
-				continue
-			}
-			s, v, d, u := f[0], f[1], f[2], f[3]
-			change.Version = string(bytes.Trim(v, "()"))
 
+		if !inside && change.parseVersionLine(b) {
 			if len(afterVersion) > 0 && change.Version == afterVersion {
 				break
 			}
-
-			change.Source = string(s)
-			change.Dist = string(bytes.TrimRight(d, ";"))
-			change.Urgency = string(bytes.TrimPrefix(u, []byte("urgency=")))
-
 			inside = true
 			continue
 		}
 
-		// try to parse author line
-		if line := bytes.TrimPrefix(b, []byte(" -- ")); inside && len(line) < len(b) {
-			start := bytes.IndexByte(line, '<')
-			end := bytes.IndexByte(line, '>')
-			if start < 0 || start >= end || end >= len(line) {
-				continue
-			}
-			update, err := time.Parse(time.RFC1123Z, string(bytes.TrimSpace(line[end+1:])))
-			if err != nil {
-				continue
-			}
-			change.Author = string(bytes.TrimSpace(line[:start]))
-			change.Email = string(line[start+1 : end-2])
-			change.Changed = update
-
+		if inside && change.parseChangedByLine(b) {
 			changelog = append(changelog, change)
 			change = Change{}
-
 			inside = false
 			continue
 		}
